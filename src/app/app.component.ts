7ecb2f6a4ec4a30543126e3f79a0846ed090bdf4
Sunil Bamre <sunil.bamre@eulogik.com>
2017-07-31 14:25:38 +0530

    my first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
+moduledId: moduled.id,
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  title = 'app';
-}
+  templateUrl:'./app'.component.html',
+  styleUrls:['./app.component.css']
+  
+   })
+export class AppComponent {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import{FormsModule} form '@anguler/forms';
 import { AppComponent } from './app.component';
-
+import { HeaderComponent } from './header/header.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { FooterComponent } from './footer/footer.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import {RouteModule,Routes } from '.@angular/router';
+import { UserService} from './user.service'
+const appRoutes:routes=[
+{
+  path:'',
+  component:LoginFromComponent 
+},
+{
+  path:'dashboard',
+  component:DashboardComponent
+}
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LoginFormComponent,
+    FooterComponent,
+    DashboardComponent
+    import:[]
   ],
   imports: [
     BrowserModule
